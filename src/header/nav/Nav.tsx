309958a02c1880f13dff0b3411dcd1675e2eecb5
Bogdan-design <bogdanbw@gmail.com>
2023-06-03 23:30:32 +0200

    NavBar2

--- src/header/nav/Nav.tsx
+++ src/header/nav/Nav.tsx
@@ -7,18 +7,42 @@ export const Nav = () => {
 
     return (
         <div className={s.nav}>
-            <a href="#ome">Home</a>
+            <Link activeClass={s.active}
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  offset={-80}
+                  duration={500}
+                  delay={0}>
+                Home
+            </Link>
             <Link activeClass={s.active}
                   to="skills"
                   spy={true}
                   smooth={true}
-                  offset={0}
+                  offset={-80}
                   duration={500}
                   delay={0}>
                 Skills
             </Link>
-            <a href="#projects">Projects</a>
-            <a href="#contacts">Contacts</a>
+            <Link activeClass={s.active}
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-80}
+                  duration={500}
+                  delay={0}>
+                Projects
+            </Link>
+            <Link activeClass={s.active}
+                  to="contacts"
+                  spy={true}
+                  smooth={true}
+                  offset={-80}
+                  duration={500}
+                  delay={0}>
+                Contacts
+            </Link>
         </div>
     );
 };
