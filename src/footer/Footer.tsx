23b7f46748298ed7265165a3ef5c84b90c57a08e
Bogdan-design <bogdanbw@gmail.com>
2023-09-04 21:43:18 +0200

    refactor

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -9,15 +9,16 @@ import telegramIcon from "../assets/image/contacts/telegram.svg";
 type IconContactsType = {
     id: number
     img: string
+    link?:string
 }
 
 export const Footer = () => {
 
     const iconsContact: IconContactsType[] = [
-        {id: 0, img: linkedinIcon},
-        {id: 1, img: githubIcon},
-        {id: 2, img: facebookIcon},
-        {id: 3, img: telegramIcon}
+        {id: 0, img: linkedinIcon,link:'https://www.linkedin.com/in/bogdan-bochkovskyi-6288a2193/'},
+        {id: 1, img: githubIcon,link:'https://github.com/Bogdan-design'},
+        {id: 2, img: facebookIcon,link:'https://www.facebook.com/bogdan.bochkovskiy'},
+        {id: 3, img: telegramIcon,link:'https://web.telegram.org/k/#-1242637873'}
     ]
 
     return (
@@ -28,7 +29,7 @@ export const Footer = () => {
                     {iconsContact.map(k => {
                         return (
                             <div key={k.id} className={s.element}>
-                                <a href="">
+                                <a href={k.link}>
                                     <img src={k.img} alt=""/>
                                 </a>
                             </div>
